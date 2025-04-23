b3b69d42c099e5d9a805b8706fedfef710b1686f
Shivam Mudotia <shivam.mudotia@gmail.com>
2025-04-23 17:09:23 +0530

    setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as f:
 
 __version__ = "0.0.0"
 
-REPO_NAME = "End-to-end-ML-Project-with-MLflow"
+REPO_NAME = "mlops_e2e_projects_mlflow"
 AUTHOR_USER_NAME = "ShivamMudotia"
 SRC_REPO = "mlProject"
 AUTHOR_EMAIL = "admin@gmail.com"
